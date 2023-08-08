60ef4d9934556c63ca0e40097a3471df0ce19739
Tejaskurule <141105444+Tejaskurule@users.noreply.github.com>
2023-08-08 09:33:12 +0530

    added descending order logic
    added descending order logic

--- ArraySort.java
+++ ArraySort.java
@@ -13,5 +13,21 @@ public class ArraySort {
         for (int i=0;i<a.length;i++){
             System.out.println(a[i]);
         }
+
+        System.out.println("given Array in Descending Order is :");
+        int temp = 0;
+        for(int i=0;i<a.length;i++){
+            for(int j=i+1;j<a.length;j++){
+                if(a[i]<a[j]){
+                    temp = a[i];
+                    a[i]= a[j];
+                    a[j]= temp;
+
+                }
+            }
+        }
+        for(int i=0;i<a.length;i++){
+            System.out.println(a[i]);
+        }
     }
 }
