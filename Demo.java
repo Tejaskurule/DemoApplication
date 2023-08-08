4f4aefcf076ccfe95c567437d5f3d77dea7f6d07
Tejaskurule <141105444+Tejaskurule@users.noreply.github.com>
2023-08-08 09:05:43 +0530

    Ticket 1: Added sout statement
    Ticket 1: Added sout statement

--- Demo.java
+++ Demo.java
@@ -1,5 +1,6 @@
 public class Demo {
     public static void main(String[] args) {
         System.out.println("This is my first class which I will commit to repo");
+        System.out.println("Program ended");
     }
 }
